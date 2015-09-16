8acf269ac511344f814b6d613ca7ed8ec0795f1d
Michael Tedeschi <mike@interactivemechanics.com>
2015-09-15 23:44:07 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,5 +4,5 @@ function public_nav_main_bootstrap() {
     $partial = array('common/menu-partial.phtml', 'default');
     $nav = public_nav_main();  // this looks like $this->navigation()->menu() from Zend
     $nav->setPartial($partial);
-    echo $nav->render();
+    return $nav->render();
 }
